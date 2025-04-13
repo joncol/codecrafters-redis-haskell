47a68e337622d892b0124ec50c07fe27a61db0e9
Jonas Collberg <jonas.collberg@gmail.com>
2025-04-13 10:30:00 +0200

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,59 @@
+# Source: https://docs.haskellstack.org/en/stable/topics/nix_integration
+
+{
+  description = "CodeCrafters: Build your own Redis";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        # Needs to match Stackage LTS version from stack.yaml snapshot.
+        hPkgs = pkgs.haskell.packages.ghc984;
+
+        myDevTools = [
+          hPkgs.fourmolu
+          hPkgs.ghc
+          hPkgs.haskell-language-server
+          pkgs.codecrafters-cli
+          pkgs.netcat-gnu
+          pkgs.pkg-config
+          pkgs.redis
+          pkgs.zlib
+          stack-wrapped
+        ];
+
+        # Wrap Stack to work with our Nix integration. We don't want to modify
+        # stack.yaml so non-Nix users don't notice anything.
+        # - no-nix: We don't want Stack's way of integrating Nix.
+        # --system-ghc    # Use the existing GHC on PATH (will come from this Nix file)
+        # --no-install-ghc  # Don't try to install GHC if no matching GHC found on PATH
+        stack-wrapped = pkgs.symlinkJoin {
+          name = "stack"; # will be available as the usual `stack` in terminal
+          paths = [ pkgs.stack ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/stack \
+              --add-flags "\
+                --no-nix \
+                --system-ghc \
+                --no-install-ghc \
+              "
+          '';
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = myDevTools;
+
+          # Make external Nix c libraries like zlib known to GHC, like
+          # pkgs.haskell.lib.buildStackProject does
+          # https://github.com/NixOS/nixpkgs/blob/d64780ea0e22b5f61cd6012a456869c702a72f20/pkgs/development/haskell-modules/generic-stack-builder.nix#L38
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath myDevTools;
+        };
+      });
+}
